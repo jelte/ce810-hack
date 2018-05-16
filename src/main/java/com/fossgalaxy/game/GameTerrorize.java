632c3b4b769e537366544d2ccf9e828c6cd5789f
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-05-16 09:40:21 +0100

    Fix worker

--- src/main/java/com/fossgalaxy/game/GameTerrorize.java
+++ src/main/java/com/fossgalaxy/game/GameTerrorize.java
@@ -5,9 +5,6 @@ import com.fossgalaxy.games.tbs.io.SettingsIO;
 
 public class GameTerrorize {
 	public static void main(String[] args) {
-
         App.run(SettingsIO.buildWithExtras("com.fossgalaxy.game"), "game.json", "game-map.json", "Player", "Player","Player");
-
     }
-
 }
