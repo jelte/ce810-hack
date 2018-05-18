adcceb58e924a58c0d9b49027fa3976f34164501
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-05-18 13:36:46 +0100

    Fix tile expansion

--- src/main/java/com/fossgalaxy/game/orders/ExpandOrder.java
+++ src/main/java/com/fossgalaxy/game/orders/ExpandOrder.java
@@ -1,5 +1,6 @@
 package com.fossgalaxy.game.orders;
 
+import com.fossgalaxy.game.TileClearer;
 import com.fossgalaxy.games.tbs.GameState;
 import com.fossgalaxy.games.tbs.entity.Entity;
 import com.fossgalaxy.games.tbs.order.Order;
@@ -21,64 +22,6 @@ public class ExpandOrder implements Order {
 
     @Override
     public void doOrder(Entity host, GameState state) {
-        List<CubeCoordinate> grounds = new ArrayList<>();
-        List<CubeCoordinate> occuppied = new ArrayList<>();
-
-        List<Entity> closest = new ArrayList<>();
-        try {
-            for (Entity entity : state.getOwnedEntities(host.getOwner())) {
-                if (state.getCalc().isVisible(state.cube2hex(host.getPos()), state.cube2hex(entity.getPos())) && (entity.getType().getName().toLowerCase().endsWith("_base") ||
-                        (entity.getType().equals(host.getType()) && !entity.equals(host)))) {
-                    closest.add(entity);
-                }
-            }
-
-            if (closest.size() < 2) {
-                return;
-            }
-            // sort by distance
-            closest.sort((a, b) -> (
-                    state.getDistance(host.getPos(), a.getPos()) < state.getDistance(host.getPos(), b.getPos()) ? -1 : 1
-            ));
-            TerrainType walkable = state.getSettings().getTerrainType("walkable");
-            TerrainType hostTerrainType = state.getSettings().getTerrainType((host.getOwner() == 0 ? "blue" : "red") + "_tile");
-
-            // Only process closest 2
-            for (Entity entity : closest) {
-                state.getCalc().drawLine(state.cube2hex(host.getPos()), state.cube2hex(entity.getPos())).forEach((tile) -> {
-                    if (state.getTerrainAt(tile.getCubeCoordinate()) == null) return;
-                    // Get all ground & contestable tiles
-                    if (state.getTerrainAt(tile.getCubeCoordinate()).equals(walkable) && !grounds.contains(tile.getCubeCoordinate())) {
-                        grounds.add(tile.getCubeCoordinate());
-                    }
-
-                    // Get all occupied tiles
-                    if (state.getTerrainAt(tile.getCubeCoordinate()).equals(hostTerrainType) && !occuppied.contains(tile.getCubeCoordinate())) {
-                        occuppied.add(tile.getCubeCoordinate());
-                    }
-                });
-            }
-            // add tiles in between
-            for (int i = 0; i < occuppied.size() - 1; i++) {
-                for (int j = i + 1; j < occuppied.size(); j++) {
-                    if (state.getCalc().isVisible(state.cube2hex(occuppied.get(i)), state.cube2hex(occuppied.get(j)))) {
-                        state.getCalc().drawLine(state.cube2hex(occuppied.get(i)), state.cube2hex(occuppied.get(j))).forEach((tile) -> {
-                            // Get all ground & contestable tiles
-                            if (state.getTerrainAt(tile.getCubeCoordinate()).equals(walkable) && !grounds.contains(tile.getCubeCoordinate())) {
-                                grounds.add(tile.getCubeCoordinate());
-                            }
-                        });
-                    }
-                }
-            }
-
-            ResourceType tiles = state.getSettings().getResourceType("tiles_"+(host.getOwner() == 0?"blue":"red"));
-            // assign terrains
-            for (int m = 0; m < quantityPerTurn && m < grounds.size(); m++) {
-                state.setTerrainAt(grounds.get(m), hostTerrainType);
-                state.setResource(0, tiles, state.getResource(0, tiles) + 1);
-                state.setResource(1, tiles, state.getResource(1, tiles) + 1);
-            }
-        } catch (NoSuchElementException e) {}
+        TileClearer.connect(host, state, quantityPerTurn);
     }
 }
